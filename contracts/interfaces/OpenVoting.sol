3a7e320631a87fe7ed7687035193c312e8f1f017
bitparadigm <vlad.lazarez@windranger.io>
2021-10-11 22:33:01 +0300

    feat: remove delegations, replace with voting power oracle provider, mocks for tests

--- contracts/interfaces/OpenVoting.sol
+++ contracts/interfaces/OpenVoting.sol
@@ -6,6 +6,6 @@ pragma solidity ^0.8.0;
  * @notice Retrieve the voting power for an account, at this moment in time.
  * @dev Used by the off-chain Snpashot voting strategy bitdao-vote
  */
-interface OpenVoting {
+interface IOpenVoting {
     function getVotes(address account) external view returns (uint256);
 }
