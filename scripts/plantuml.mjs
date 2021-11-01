6b809e36a1715b4b8b3b807e2d27a3768ae798eb
Shahan Khatchadourian <shahan.k.code@gmail.com>
2021-11-01 14:12:13 -0400

    [Feature] Output of built images into docs folder

--- scripts/plantuml.mjs
+++ scripts/plantuml.mjs
@@ -1,36 +1,40 @@
 import plantuml from 'node-plantuml';
 import fs from 'fs';
 import {readFile, mkdir} from 'fs/promises';
-
-const outputLoc = "./build/";
+import path from 'path';
 
 const myArgs = process.argv.slice(2);
 
-try {
-  await mkdir(outputLoc);
-} catch {
-//  nop if directory doesn't exist
-}
-
 for (const arg of myArgs) {
-  const path = arg.split("/");
-  const fileroot = path[path.length - 1].split(".")[0];
+  const pathObj = path.parse(arg);
 
   const docContent = await readFile(arg, "utf8");
   const len = (docContent.match(/newpage/g) || []).length;
+
+  const targetDir = pathObj.dir + path.sep + 'images' + path.sep + pathObj.name
+      + path.sep;
+
+  try {
+    await mkdir(targetDir, {recursive: true});
+  } catch (err) {
+//  nop if directory doesn't exist
+  }
 
   for (var i = 0; i <= len; i++) {
     (function () {
       const j = i;
       const gen = plantuml.generate(docContent, {"pipeimageindex": j});
-      const outputFile = outputLoc + fileroot + "-plantuml-image-" + j + ".png";
+
+      const outputFile = targetDir + pathObj.base + "-plantuml-image-" + j
+          + ".png";
 
       console.log("starting:" + outputFile);
 
       gen.out.pipe(fs.createWriteStream(
           outputFile)).on('finish',
           _ => {
-            console.log(arg + ": generated image " + j + " of " + len + " to " + outputFile);
+            console.log(arg + ": generated image " + j + " of " + len + " to "
+                + outputFile);
           })
     })();
   }
